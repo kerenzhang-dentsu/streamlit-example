88afb32dc11089d24c505c8ad27a03009c0c3881
Keren Zhang <keren.zhang@dentsu.com>
2022-10-11 14:48:03 -0400

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import streamlit as st
 from pathlib import Path
 from PIL import Image
 import numpy as np
+import plotly.express as px
 
 
 # Data Config
@@ -65,7 +66,47 @@ if page == "Lark":
             df = pd.read_csv("BofA_Merill_Edge_Unit_and_Performance_Data_09092022.csv")
             if dataset_name == 'BOA Demo':      
                 df
-
+                columns = list(df.columns)
+                col1,col2 = st.columns(2)
+                with col1:
+                    date_col = st.selectbox("Select Data Column", index=0, options=columns, key="date")
+                with col2:
+                    metric_col = st.selectbox("Select Values Column", index=4, options=columns, key="values")
+                df = prep_data(df,date_col,metric_col)
+                output = 0
+                fig = px.line(df,
+                              x='ds',
+                              y='y',
+                              color_discrete_sequence=["#ff0066", "#0477BF", "#ff9933", "#337788","#429e79", "#474747", "#f7d126", "#ee5eab", "#b8b8b8"]
+                              )
+                fig.update_xaxes(
+                            rangeslider_visible=True,
+                            rangeselector=dict(
+                                buttons=list(
+                                    [
+                                        dict(count=7, label="1w", step="day", stepmode="backward"),
+                                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                                        dict(count=3, label="3m", step="month", stepmode="backward"),
+                                        dict(count=6, label="6m", step="month", stepmode="backward"),
+                                        dict(count=1, label="YTD", step="year", stepmode="todate"),
+                                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                                        dict(step="all"),
+                                    ]
+                                )
+                            ),
+                        )
+                fig.update_layout(
+                            yaxis_title=metric_col,
+                            xaxis_title=date_col,
+                            legend_title_text="",
+                            height=500,
+                            width=800,
+                            title_text="Time Series Preview",
+                            title_x=0.5,
+                            title_y=1,
+                            hovermode="x unified",
+                        )
+                st.plotly_chart(fig, use_container_width=True)
 
  
 
