444b0597b1dbdc696cf3a3776baa9e01eabc5119
Keren Zhang <keren.zhang@dentsu.com>
2022-10-13 11:22:54 -0400

    11

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,34 @@ from pathlib import Path
 from PIL import Image
 import numpy as np
 import plotly.express as px
-import subprocess
-import sys
+from lark.base import Setup
+from lark.models.arima import ARIMAParams, ARIMAModel
+from lark.models.prophet import ProphetParams, ProphetModel
+from lark.models.linear_regression import LinearRegressionParams, LinearRegressionModel
+from lark.models.random_forest_regressor import RandomForestRegressorParams, RandomForestRegressorModel
+from lark.models.extreme_gradient_boosting import ExtremeGradientBoostingParams, ExtremeGradientBoostingModel
+from lark.models.gradient_boosting import GradientBoostingParams, GradientBoostingModel
+from lark.models.cat_boost_regressor import CatBoostRegressorParams, CatBoostRegressorModel
+from lark.models.light_gradient_boosting_machine import LightGradientBoostingMachineParams, LightGradientBoostingMachineModel
+from lark.models.extra_trees_regressor import ExtraTreesRegressorParams, ExtraTreesRegressorModel
+from lark.models.ada_boost import AdaBoostParams, AdaBoostModel
+from lark.models.ridge_regression import RidgeRegressionModel, RidgeRegressionParams
+from lark.models.bayesian_ridge_regression import BayesianRidgeRegressionModel, BayesianRidgeRegressionParams
+from lark.models.lasso_regression import LassoRegressionParams, LassoRegressionModel
+from lark.models.theil_sen_regression import TheilSenRegressionParams, TheilSenRegressionModel
+from lark.models.least_angle_regression import LeastAngleRegressionParams, LeastAngleRegressionModel
+from lark.models.lasso_least_angle_regression import LassoLeastAngleRegressionModel, LassoLeastAngleRegressionParams
+from lark.models.decision_tree_regressor import DecisionTreeRegressorParams, DecisionTreeRegressorModel
+from lark.models.huber_regressor import HuberRegressorParams, HuberRegressorModel
+from lark.models.random_sample_consensus import RandomSampleConsensusParams, RandomSampleConsensusModel
+from lark.models.orthogonal_matching_pursuit import OrthogonalMatchingPursuitModel, OrthogonalMatchingPursuitParams
+from lark.models.passive_aggressiver_regressor import PassiveAggressiveRegressorParams, PassiveAggressiverRegressorModel
+from lark.models.elastic_net import ElasticNetParams, ElasticNetModel
+from lark.models.k_nerest_neighbors_regressor import KNerestNeighborsRegressorParams, KNerestNeighborsRegressorModel
+from lark.models.support_vector_machine import SupportVectorMachineParams, SupportVectorMachineModel
+
+# import subprocess
+# import sys
 #manually install xlrd and lark because dockerfile cannot install them somehow
 # def install(package):
 #     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
@@ -49,6 +75,40 @@ def mape(actual, pred):
     actual, pred = np.array(actual), np.array(pred)
     return np.mean(np.abs(actual - pred) / actual)
 
+def add_seasonal_features(
+    data,
+    datetime_col
+):
+    data['year'] = data[datetime_col].dt.year
+    data['quarter'] = data[datetime_col].dt.quarter
+    data['month'] = data[datetime_col].dt.month
+    data['weekofyear'] = data[datetime_col].dt.isocalendar().week.astype(np.int64)
+    data['dayofyear'] = data[datetime_col].dt.dayofyear
+    data['dayofmonth'] = data[datetime_col].dt.day
+    data['dayofweek'] = data[datetime_col].dt.weekday
+    data['weekday'] = (data['dayofweek'] < 5) * 1  # 0 == monday
+    data['weekend'] = (data['weekday'] == 0) * 1
+    return data
+
+def create_forecast_data(
+    last_date_in_period,
+    forecasting_length_days=31,
+    datetime_col='date',
+    create_seasonal_features=True,
+):
+    forecasting_start_date = last_date_in_period + pd.DateOffset(1)
+ 
+    date_range = pd.date_range(forecasting_start_date,
+                               periods=forecasting_length_days)
+ 
+    forecast = pd.DataFrame({datetime_col: date_range})
+ 
+    if create_seasonal_features == True:
+        forecast = add_seasonal_features(forecast, datetime_col)
+
+    return forecast
+
+
 # Page Info - Causal Impact Analysis
 #st.sidebar.image(load_image('Clearchoice_logo.png'), use_column_width=True)
 tabs = ["Lark"]
@@ -82,11 +142,12 @@ if page == "Lark":
                 with col1:
                     date_col = st.selectbox("Select Data Column", index=0, options=columns, key="date")
                 with col2:
-                    metric_col = st.selectbox("Select Values Column", index=4, options=columns, key="values")
+                    columns_new = columns.remove(date_col)
+                    metric_col = st.selectbox("Select Values Column", index=4, options=columns_new, key="values")
 
-                df = prep_data(df,date_col,metric_col)
+                df_show = prep_data(df,date_col,metric_col)
                 output = 0
-                fig = px.line(df,
+                fig = px.line(df_show,
                               x='ds',
                               y='y',
                               color_discrete_sequence=["#ff0066", "#0477BF", "#ff9933", "#337788","#429e79", "#474747", "#f7d126", "#ee5eab", "#b8b8b8"]
@@ -120,14 +181,15 @@ if page == "Lark":
                         )
                 st.plotly_chart(fig, use_container_width=True)
 
-    st.subheader("2. Parameters Configuration")
+    st.subheader("2. Model Configuration")
     with st.form("config"):
         with st.container():
-            st.write('In this section you can choose the model you want to use.')
+            st.write('In this section you can choose the model, target and parameters you want to use.')
             with st.expander("Models"):
                 model_list = [
                                 'Arima', 
                                 'Prophet', 
+                                'Linear Regression'
                                 'Random Forest', 
                                 'Extreme Gradient Boosting', 
                                 'Gradient Boosting', 
@@ -150,7 +212,16 @@ if page == "Lark":
                                 'K-Neighbors Regression',
                                 'Support Vector Machine'
                                 ]
-                selected_model = st.selectbox(label='Select model', options=model_list)
+                selected_model = st.selectbox(label = 'Select model', options = model_list)
+            # with st.expander("Target"):
+            #     target_list = [
+            #                     'Cost',
+            #                     'Clicks',
+            #                     'Impressions',
+            #                     'Accounts_Opened',
+            #                     'Funded_Units'
+            #                     ]
+            #     selected_target = st.selectbox(label = 'Select target', options = target_list)
             with st.expander("Parameters:"):
                 st.write('In this section it is possible to modify pamameters of your model.')
                 if selected_model == 'Arima':
@@ -216,3 +287,36 @@ if page == "Lark":
     with st.container():
         st.subheader("3. Forecast")
         st.write("Fit the model on the data and generate future prediction.")
+        if dataset_name == 'BOA Demo':
+            if st.checkbox("Initialize Model (Fit)", key="fit"):
+                train_data_ts = Setup(data = df_show[['ds','y']], time_col_name = 'ds')
+                df_tmp = add_seasonal_features(df, 'Date')
+                df_tmp_new = df_tmp.rename(columns={'Date':'ds', metric_col:'y'}).copy()
+                if metric_col == 'Cost':
+                    train_data = Setup(data=df_tmp_new[['ds', 'y','year', 'quarter', 'month', 'weekofyear', 'dayofmonth', 'dayofyear','dayofweek', 'weekday', 'weekend']], time_col_name="ds")
+                elif metric_col == 'Clicks':
+                    train_data = Setup(data=df_tmp_new[['ds', 'y','year', 'quarter', 'month', 'weekofyear', 'dayofmonth', 'dayofyear','dayofweek', 'weekday', 'weekend', 'Cost']], time_col_name="ds")
+                elif metric_col == 'Impressions':
+                    train_data = Setup(data=df_tmp_new[['ds', 'y','year', 'quarter', 'month', 'weekofyear', 'dayofmonth', 'dayofyear','dayofweek', 'weekday', 'weekend', 'Cost']], time_col_name="ds")
+                elif metric_col == 'Accounts_Opened':
+                    train_data = Setup(data=df_tmp_new[['ds', 'y','year', 'quarter', 'month', 'weekofyear', 'dayofmonth', 'dayofyear','dayofweek', 'weekday', 'weekend', 'Cost']], time_col_name="ds")
+                elif metric_col =='Funded_Units':
+                    train_data = Setup(data=df_tmp_new[['ds', 'y','year', 'quarter', 'month', 'weekofyear', 'dayofmonth', 'dayofyear','dayofweek', 'weekday', 'weekend', 'Cost']], time_col_name="ds")
+
+
+
+                if selected_model == 'Arima':
+                    params = ARIMAParams(p = p_scale, d = d_scale, q = q_scale)
+                    m = ARIMAModel(data = train_data_ts, params = params)
+                elif selected_model == 'Prophet':
+                    params = ProphetParams(weekly_seasonality = weekly_scale,
+                                            yearly_seasonality = yearly_scale,
+                                            seasonality_prior_scale = seasonality_scale,
+                                            changepoint_range = changepoint_scale)
+                    m = ProphetModel(data = train_data_ts, params = params)
+                elif selected_model == 'Linear Regression':
+                    params = LinearRegressionParams()
+                    m = LinearRegressionModel(data = )
+
+
+
